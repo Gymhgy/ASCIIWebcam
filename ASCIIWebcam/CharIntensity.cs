f46baeac27f212adb46cb0f30e96d6aea65753c3
Gymhgy <49801033+Gymhgy@users.noreply.github.com>
2021-11-19 23:57:20 -0800

    shape based first try

--- ASCIIWebcam/CharIntensity.cs
+++ ASCIIWebcam/CharIntensity.cs
@@ -7,8 +7,24 @@ using System.Threading.Tasks;
 namespace ASCIIWebcam {
     struct CharIntensity {
 
-        public int rightIntensity, leftIntensity, topIntensity, bottomIntensity, centerIntensity;
+        public readonly double rightIntensity, leftIntensity, topIntensity, bottomIntensity, centerIntensity;
+        public readonly char Char;
 
+        public CharIntensity(char Char, double rightIntensity, double leftIntensity, double topIntensity, double bottomIntensity, double centerIntensity) {
+            this.Char = Char;
+            this.rightIntensity = rightIntensity;
+            this.leftIntensity = leftIntensity;
+            this.topIntensity = topIntensity;
+            this.bottomIntensity = bottomIntensity;
+            this.centerIntensity = centerIntensity;
+        }
 
+        public double DistanceMetric(CharIntensity compar) {
+            return Math.Abs(rightIntensity - compar.rightIntensity)
+                + Math.Abs(leftIntensity - compar.leftIntensity)
+                + Math.Abs(topIntensity - compar.topIntensity)
+                + Math.Abs(bottomIntensity - compar.bottomIntensity)
+                + Math.Abs(centerIntensity - compar.centerIntensity);
+        }
     }
 }
--- ASCIIWebcam/Program.cs
+++ ASCIIWebcam/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Drawing;
 using System.IO;
 using System.Text;
@@ -13,15 +15,20 @@ namespace ASCIIWebcam {
         const string map = "@#%xo;:,. ";
 
         static void Main(string[] args) {
-            int cols = 160;
-            if (args.Length > 0) {
-                int.TryParse(args[0], out cols);
-            }
+            int fontSize = 6;
+            SKFont font = new SKFont(SKTypeface.FromFamilyName("Consolas"), fontSize);
+            SKPaint paint = new SKPaint(font);
 
-            double ratio = 0.43;
+            paint.TextAlign = SKTextAlign.Center;
+            paint.Color = SKColor.Parse("#000000");
+            var metrics = paint.FontMetrics;
+            int width = fontSize / 2;
+            int height = fontSize;
 
-            int tileWidth=0, tileHeight=0, rows=0;
-            bool init = false;
+            int leftRightSize = width / 3;
+            int topBottomSize = height / 3;
+
+            ComputeIntensityMap(paint, leftRightSize, topBottomSize);
             VideoCapture capture = new VideoCapture();
             while (true) {
                 Console.SetCursorPosition(0, 0);
@@ -29,55 +36,89 @@ namespace ASCIIWebcam {
                     Image<Gray, byte> img = mat.ToImage<Gray, byte>();
 
                     byte[,,] data = img.Data;
-                    if (!init) {
-                        tileWidth = img.Cols / cols;
-                        tileHeight = (int)(tileWidth / ratio);
-                        rows = img.Height / tileHeight;
-                        ComputeIntensityMap();
-                    }
-                    for (int i = 0; i < rows; i++) {
-                        int y = i * tileHeight, height = i != rows - 1 ? tileHeight : img.Rows - y;
+                    int imgHeight = img.Height, imgWidth = img.Width;
+                    int area = width * height;
+                    for (int i = 0; i < imgHeight; i += height) {
+                        int rectHeight = i + height > imgHeight ? imgHeight - i : height;
 
                         StringBuilder line = new StringBuilder();
-                        for (int j = 0; j < cols; j++) {
-                            int x = j * tileWidth, width = j != cols - 1 ? tileWidth : img.Cols - x;
+                        for (int j = 0; j < imgWidth; j += width) {
+                            int rectWidth = j + width > imgWidth ? imgWidth - j : width;
 
-                            Image<Gray, byte> tile = img.Copy(new Rectangle(x, y, width, height));
+                            double rightIntensity = 0, leftIntensity = 0, topIntensity = 0, bottomIntensity = 0, centerIntensity = 0;
+                            for (int row = i; row < i + rectHeight; row++) {
+                                for (int col = j; col < j + rectWidth; col++) {
+                                    bool center = true;
+                                    if (row < topBottomSize) { center = false; topIntensity += data[row, col, 0]; }
+                                    if (row >= height - topBottomSize) { center = false; bottomIntensity += data[row, col, 0]; }
 
-                            double mean = CvInvoke.Mean(tile).V0;
-                            line.Append(map[(int)((mean / 255) * (map.Length - 1))]);
+                                    if (col < leftRightSize) { center = false; leftIntensity += data[row, col, 0]; }
+                                    if (col >= width - leftRightSize) { center = false; rightIntensity += data[row, col, 0]; }
+
+                                    if (center) centerIntensity += data[row, col, 0];
+                                }
+                            }
+                            rightIntensity /= area;
+                            leftIntensity /= area;
+                            topIntensity /= area;
+                            bottomIntensity /= area;
+                            centerIntensity /= area;
+                            var intensity = new CharIntensity((char)1, rightIntensity, leftIntensity, topIntensity, bottomIntensity, centerIntensity);
+                            char c = intensities.OrderByDescending(charIntensity => charIntensity.DistanceMetric(intensity)).First().Char;
+                            line.Append(c.ToString());
                         }
                         Console.WriteLine(line);
                     }
                 }
             }
-
         }
 
-        static void ComputeIntensityMap() {
-            float fontSize = 18f;
-            SKFont font = new SKFont(SKTypeface.FromFamilyName("Consolas"), fontSize);
-            SKPaint paint = new SKPaint(font);
-            
-            paint.TextAlign = SKTextAlign.Center;
-            paint.Color = SKColor.Parse("#000000");
+        static List<CharIntensity> intensities = new List<CharIntensity>();
+
+        static void ComputeIntensityMap(SKPaint paint, int leftRightSize, int topBottomSize) {
             var metrics = paint.FontMetrics;
             var descent = metrics.Descent;
-            int width = (int)Math.Ceiling(paint.MeasureText("!")) + 1;
-            int height = (int)(fontSize * 4 / 3);
+            int fontSize = (int)paint.ToFont().Size;
+            int width = fontSize / 2;
+            int height = fontSize;
+
             Console.WriteLine(width);
-            SKImageInfo imageInfo = new SKImageInfo(width, height);
-            for (char i = ' '; i < 127; i++) {
-                using (SKSurface surface = SKSurface.Create(imageInfo)) {
-                    SKCanvas canvas = surface.Canvas;
+            Console.WriteLine(metrics.XHeight);
+            Console.WriteLine(paint.MeasureText("!"));
+
+            SKImageInfo imageInfo = new SKImageInfo(width, height, SKColorType.Gray8);
+
+            using (SKBitmap bitmap = new SKBitmap(imageInfo)) {
+                SKCanvas canvas = new SKCanvas(bitmap);
+                for (char i = ' '; i < 127; i++) {
                     canvas.Clear(SKColors.White);
                     canvas.DrawText(i.ToString(), width / 2, height-descent, paint);
-                    using (var image = surface.Snapshot())
-                    using (var data = image.Encode(SKEncodedImageFormat.Png, 80))
-                    using (var stream = File.OpenWrite((int)i+"_abc.png")) {
-                        // save the data to a stream
-                        data.SaveTo(stream);
+
+                    double rightIntensity = 0, leftIntensity = 0, topIntensity = 0, bottomIntensity = 0, centerIntensity = 0;
+                    int area = width * height;
+                    unsafe {
+                        byte* ptr = (byte*)bitmap.GetPixels().ToPointer();
+                        for (int row = 0; row < width; row++) {
+                            for(int col = 0; col < height; col++) {
+                                bool center = true;
+                                if (row < topBottomSize) { center = false; topIntensity += *ptr; }
+                                if (row >= height - topBottomSize) { center = false; bottomIntensity += *ptr; }
+
+                                if (col < leftRightSize) { center = false; leftIntensity += *ptr; }
+                                if (col >= width - leftRightSize) { center = false; rightIntensity += *ptr; }
+
+                                if (center) centerIntensity += *ptr;
+
+                                ptr++;
+                            }
+                        }
                     }
+                    rightIntensity /= area;
+                    leftIntensity /= area;
+                    topIntensity /= area;
+                    bottomIntensity /= area;
+                    centerIntensity /= area;
+                    intensities.Add(new CharIntensity(i, rightIntensity, leftIntensity, topIntensity, bottomIntensity, centerIntensity));
                 }
             }
         }
